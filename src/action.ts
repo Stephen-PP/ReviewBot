7f88e79d109cc4f6b46fb518fd15fda28f88590f
Stephen-PP <pierrepaulstephen@gmail.com>
2024-05-16 16:42:05 -0400

    basic setup

---
+++ src/action.ts
@@ -0,0 +1,23 @@
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+async function run() {
+    // Get current PR
+    const pr = github.context.payload.pull_request
+    if (!pr) {
+        core.setFailed('No pull request found.')
+        return
+    }
+
+    // Get the differences in the current PR
+    const client = github.getOctokit(core.getInput('github-token'))
+    const diff = await client.rest.pulls.listFiles({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pr.number
+    })
+
+    core.info(JSON.stringify(diff))
+}
+
+run()
